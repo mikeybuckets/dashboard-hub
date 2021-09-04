4c8c08d86cd8b3f16b7d93eae36e0e4cfb7174bc
DivOpsUS <admin@divops.us>
2021-09-04 02:24:25 -0700

    eon

---
+++ .history/src/assets/images/index_20210904021035.js
@@ -0,0 +1,33 @@
+import profile1 from "./users/profile-1.jpg"
+import profile2 from "./users/profile-2.jpg"
+import profile3 from "./users/profile-3.jpg"
+import profile4 from "./users/profile-4.jpg"
+import profile5 from "./users/profile-5.jpg"
+import profile6 from "./users/profile-6.jpg"
+import profile7 from "./users/profile-7.jpg"
+import profile8 from "./users/profile-8.jpg"
+import img1 from "./product/img-1.png"
+import img2 from "./product/img-2.png"
+import img3 from "./product/img-3.png"
+import img4 from "./product/img-4.png"
+import img5 from "./product/img-5.png"
+import img6 from "./product/img-6.png"
+import img7 from "./product/img-7.png"
+
+export default {
+  profile1,
+  profile2,
+  profile3,
+  profile4,
+  profile5,
+  profile6,
+  profile7,
+  profile8,
+  img1,
+  img2,
+  img3,
+  img4,
+  img5,
+  img6,
+  img7,
+}
---
+++ .history/src/assets/images/index_20210904021039.js
@@ -0,0 +1,33 @@
+import profileImage1 from "./users/profileImage-1.jpg"
+import profileImage2 from "./users/profileImage-2.jpg"
+import profileImage3 from "./users/profileImage-3.jpg"
+import profileImage4 from "./users/profileImage-4.jpg"
+import profileImage5 from "./users/profileImage-5.jpg"
+import profileImage6 from "./users/profileImage-6.jpg"
+import profileImage7 from "./users/profileImage-7.jpg"
+import profileImage8 from "./users/profileImage-8.jpg"
+import img1 from "./product/img-1.png"
+import img2 from "./product/img-2.png"
+import img3 from "./product/img-3.png"
+import img4 from "./product/img-4.png"
+import img5 from "./product/img-5.png"
+import img6 from "./product/img-6.png"
+import img7 from "./product/img-7.png"
+
+export default {
+  profileImage1,
+  profileImage2,
+  profileImage3,
+  profileImage4,
+  profileImage5,
+  profileImage6,
+  profileImage7,
+  profileImage8,
+  img1,
+  img2,
+  img3,
+  img4,
+  img5,
+  img6,
+  img7,
+}
